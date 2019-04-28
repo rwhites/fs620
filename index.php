e6a99a7a8c844a367ea3a3506edf983a7bf6042a
Robert Whiteside <49750460+rwhites@users.noreply.github.com>
2019-04-28 15:48:17 +0000

    Adding New Index

--- index.php
+++ index.php
@@ -8,34 +8,86 @@ $redir = $coli[rand(0,(count($coli)-1))];
 
 if(empty($_GET['id']))
 {
-echo '<!DOCTYPE html>
-<html style="height:100%">
+echo '<html>
 <head>
-<title> 404 Not Found :(</title>
-<meta name="description" content="Safely store and share your photos, videos, files and more in the cloud. Your first 15 GB of storage are free with a Google account."/>
-<link rel="canonical" href="https://sarfile.me/" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:type" content="website" />
-<meta property="og:title" content="404 Not Found :(" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="theme-color" content="#d6c9a5" />
+<meta name="msapplication-navbutton-color" content="#d6c9a5" />
+<meta name="msapplication-TileColor" content="#d6c9a5"/>
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="#d6c9a5" />
+<title>404 Not Found :(</title>
 <meta property="og:description" content="Safely store and share your photos, videos, files and more in the cloud. Your first 15 GB of storage are free with a Google account." />
-<meta property="og:url" content="https://sarfile.me/" />
 <meta property="og:site_name" content="404 Not Found :(" />
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:description" content="Safely store and share your photos, videos, files and more in the cloud. Your first 15 GB of storage are free with a Google account." />
-<meta name="twitter:title" content="SaRFile - Safely store and share your photos, videos, files and more in the cloud. Your first 15 GB of storage are free with a Google account." />
+<meta name="twitter:title" content="FileSystem - Safely store and share your photos, videos, files and more in the cloud. Your first 15 GB of storage are free with a Google account." />
 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link href=\'https://fonts.googleapis.com/css?family=Ubuntu\' rel=\'stylesheet\'>
+<link href=\'https://fonts.googleapis.com/css?family=Fredericka the Great\' rel=\'stylesheet\'>
+<style type="text/css">
+body{
+    background-image: url(/background.jpg);
+    background-size: cover;
+    margin-top: 10%;
+    color: #000;
+    text-shadow: 0 0 6px #000;
+  }
+  a, a:visited {
+    color: #FFF;
+    text-shadow: 0 0 6px #e1a74e;
+  }
+  .link{
+    text-decoration: none;
+font-family: Ubuntu;
+text-shadow: 0 0 6px #000;
+      }
+  .panel {
+    background-color: #d6c9a585;
+    color: #FFF;
+    border: solid 1px #d6c9a585;
+    padding: 4px;
+    width: 60%;
+    border-radius: 7px;
+
+} 
+
+.boton{
+  background-color: #D4A54D85;
+    border-color: transparent;
+    color: #fff;
+    border-radius: 5px;
+    width: 50%;
+    height: auto;
+    margin: 25px 25px 25px 25px;
+    text-align: center; 
+    vertical-align: middle;
+    line-height: 62px;
+}
+.boton.is-hovered, .boton:hover {
+    background-color: #D4A54D;
+    color: #FFF;
+    border: solid 1px #FFF;
+.   border-radius: 5px;
+    width: 50%;
+    height: auto;
+    margin: 25px 25px 25px 25px;
+    text-align: center; 
+    vertical-align: middle;
+    line-height: 62px;
+}
+</style>
 </head>
-<body style="color: #444; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:50%; background-color: #fff;">
-<div style="height:auto; min-height:100%; ">
-<div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 2%; left:50%;">
-        <img src="https://img.sardeath.com/icon/null.png" alt="nulled"/><br/>
-        <h1 style="margin:0; font-size:100px; line-height:80px; font-weight:bold;">404 Not Found</h1>
-<h2 style="margin-top:20px;font-size: 30px;">Error Database Not Connect!</h2>
-</div></div>
+<body>
+<center>
+<div class="panel">
+<h2 style="font-size:70px;text-shadow: 0 0 6px #000;font-family: Fredericka the Great">404 NOT FOUND</h2>
+<h2 style="font-family: Ubuntu">Error Database Not Connect With A System<br/>Please Comeback Later...</h2>
+</div>
+</center>
 </body>
-</html>
-';
+</html>';
 }else{
 $uri = isset($_GET['id']) ? $_GET['id']: null;
 $url = hex2bin($uri);
